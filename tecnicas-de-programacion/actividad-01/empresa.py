13c0221f703641849bfb829c9b41c6805a442d3a
Chaves, Leonel Abel <lchaves@fi.uba.ar>
2023-05-22 22:51:50 -0300

    Se define función para validar número flotante

--- tecnicas-de-programacion/actividad-01/empresa.py
+++ tecnicas-de-programacion/actividad-01/empresa.py
@@ -1,3 +1,34 @@
+from re import sub
+
+
+REGEX_DE_NUMERO_FLOTANTE: str = '[+-]?([0-9]+([.][0-9]*)?|[.][0-9]+)'
+
+
+def es_numero_flotante_valido(entrada: str) -> bool:
+    """
+    PRE: 'entrada', debe ser una variable de tipo string
+    POST: Evalua si la 'entrada' dada, tiene el formato de un número flotante
+          y si el mismo, es mayor a 0
+    """
+
+    numero: int = int()
+    numero_formateado: str = sub(REGEX_DE_NUMERO_FLOTANTE, '', entrada)
+    flag_numero_flotante_valido: False
+
+    if numero_formateado:
+        numero = int(numero_formateado)
+
+        if numero > 0:
+            flag_numero_flotante_valido = True
+
+        else:
+            print('\n¡Sólo se pueden ingresar numeros positivos!')
+    else:
+        print('\n¡Sólo se pueden ingresar numeros enteros o flotantes!')
+    
+    return flag_numero_flotante_valido
+
+
 def main() -> None:
     pass
 
